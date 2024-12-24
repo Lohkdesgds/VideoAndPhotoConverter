8220d2c6833bcb17ab5004ace25dceb5e508f7fb
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2024-12-24 15:00:30 -0300

    First commit, CMake with sub directory for terminal version, no common code for now, only CURL library installed

---
+++ build/cmd_project/CMakeFiles/vpc_cmd.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vpc_cmd.dir/src/main.cpp.o"
+  "CMakeFiles/vpc_cmd.dir/src/main.cpp.o.d"
+  "vpc_cmd"
+  "vpc_cmd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vpc_cmd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
